7f406b2d954ce51a5e3bafa8f3e6315ffe7de87d
lo0oleta <alaa.alnous@gmail.com>
2023-11-13 14:52:34 +0000

    NT-84 [Add the new changes]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +25,7 @@ export default function RootLayout({
         <main className="flex-grow">
         {children}
         </main>
-      {/* <Footer/> */}
+      <Footer/>
         </div>
 
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Banner from "@/components/Banner";
+import Container from "@/components/Container";
 
 export default function Home() {
   return (
-    <div >
-      Nere
+    <div className="p-8" >
+        <div>
+          <Banner/>
+        </div>
     </div>
   )
 }
