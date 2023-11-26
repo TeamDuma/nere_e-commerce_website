7ea4350106c9c1a5377ea3c375b56b76dbc147c9
lo0oleta <alaa.alnous@gmail.com>
2023-11-26 08:40:24 +0000

    add the sticky model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,130 @@
 "use client"; // This is a client component 👈🏽
 
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import FeaturedProducts from './component/FeaturedProducts';
-import TwoColumnLayout from './component/TwoColumnLayout';
-import Title from './component/common/ Title';
-import Hero from './component/common/Hero';
-import ThreeSectionRow from './component/common/ThreeSectionRow';
-import TwoSectionRow from './component/common/TwoSectionRow';
-import Container from './component/common/Container';
-import SliderComponent from './component/SliderComponent';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FeaturedProducts from "./component/FeaturedProducts";
+import TwoColumnLayout from "./component/TwoColumnLayout";
+import Title from "./component/common/ Title";
+import Hero from "./component/common/Hero";
+import ThreeSectionRow from "./component/common/ThreeSectionRow";
+import TwoSectionRow from "./component/common/TwoSectionRow";
+import Container from "./component/common/Container";
+import SliderComponent from "./component/SliderComponent";
+import Link from "next/link";
+import OngoingPurchases from "./ongoingPurchases/page";
+import { useState } from "react";
+import SideModal from "./component/SideModal";
 
 const products = [
-  { name: 'Product 1', price: 20, image: 'product1.jpg' },
-  { name: 'Product 2', price: 30, image: 'product2.jpg' },
+  { name: "Product 1", price: 20, image: "product1.jpg" },
+  { name: "Product 2", price: 30, image: "product2.jpg" },
   // Add more products as needed
 ];
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleSeeMoreClick = () => {
-    // Navigate to another page or route
-    window.location.href = '/Products';
+  // Function to toggle the modal
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <Container>
-      <TwoSectionRow />
-      <Hero images={[
-        {
-          id: '1',
-          url: 'https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/b1d1axq63tqihbmvgq7l.png',
-        },
-        {
-          id: '2',
-          url: 'https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/jqako60whhrlvhmdsu0k.png',
-        },
-        {
-          id: '3',
-          url: 'https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/auskeubpqss2wv6qoacp.png',
-        },
-      ]}/>
-      <ThreeSectionRow/>
-<SliderComponent/>
-<Title text="Ongoing Groups" color="#298592" />
-{/* <OngoingPurchases/> */}
-<TwoColumnLayout/>
-<FeaturedProducts/>
-<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-      <button
-        className="rounded-lg shadow text-center text-white text-base font-semibold py-3"
-        style={{ background: "#298592", width: '150px', marginTop: '9px' }}
-        onClick={handleSeeMoreClick}
+    <>
+      <Container>
+        <TwoSectionRow />
+        <Hero
+          images={[
+            {
+              id: "1",
+              url: "https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/b1d1axq63tqihbmvgq7l.png",
+            },
+            {
+              id: "2",
+              url: "https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/jqako60whhrlvhmdsu0k.png",
+            },
+            {
+              id: "3",
+              url: "https://res.cloudinary.com/dshiwa02i/image/upload/v1691404789/auskeubpqss2wv6qoacp.png",
+            },
+          ]}
+        />
+        <ThreeSectionRow />
+        <SliderComponent />
+        <Title text="Ongoing Groups" color="#298592" />
+        <OngoingPurchases />
+
+        <TwoColumnLayout />
+        <FeaturedProducts />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "10vh",
+          }}
+        >
+          <Link href="/products">
+            <div
+              className="rounded-lg shadow text-center text-white text-base font-semibold py-3"
+              style={{
+                background: "#298592",
+                width: "150px",
+                marginTop: "9px",
+              }}
+            >
+              View All Items
+            </div>
+          </Link>
+        </div>
+      </Container>
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          transform: "translateY(-50%)",
+          right: 0,
+          zIndex: 1000,
+        }}
       >
-        View All Items
-      </button>
-    </div>
-      
-    </Container>
+        <div
+          // onClick={toggleModal}
+          className="rounded-lg shadow text-center text-white text-base font-semibold py-3"
+          style={{
+            background: "#F58929",
+            width: "150px", // Set fixed width
+            height: "80px", // Set fixed height
+            marginTop: "9px",
+            cursor: "pointer",
+          }}
+        >
+          Give it fixed width and height
+        </div>
+      </div>
+
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          transform: "translateY(-50%)",
+          left: 0,
+          zIndex: 1000,
+        }}
+      >
+        <div
+          onClick={toggleModal}
+          className="rounded-lg shadow text-center text-white text-base font-semibold py-3"
+          style={{
+            background: "#F58929",
+            width: "150px",
+            marginTop: "9px",
+            cursor: "pointer",
+            height: "80px",
+          }}
+        >
+          {} Ongoing Purchases near me{" "}
+        </div>
+      </div>
+      {isModalOpen && <SideModal closeModal={toggleModal} />}
+    </>
   );
 }
