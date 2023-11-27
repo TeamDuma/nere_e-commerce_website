e70b3ce8d561a27fc7a13f72dad240479c9ea79d
lo0oleta <alaa.alnous@gmail.com>
2023-11-27 14:50:26 +0000

    start from the beginning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from './component/common/header/Header'
-import Footer from './component/common/footer/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Nere',
-  description: 'Nere E-commerce Website',
-}
+  title: "Nere",
+  description: "Nere E-commerce Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
-
-        <Header/>
-        <main className="flex-grow">
-        {children}
-        </main>
-      <Footer/>
+          <main className="flex-grow">{children}</main>
         </div>
-
-        </body>
+      </body>
     </html>
-  )
+  );
 }
