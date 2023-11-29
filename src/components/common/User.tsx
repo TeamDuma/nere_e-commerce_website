9a2a1ddd8ff82130c017eb818f6f43aa3ae20f6a
lo0oleta <alaa.alnous@gmail.com>
2023-11-29 11:51:19 +0000

    add svg icons

---
+++ src/components/common/User.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function UserIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="24"
+      fill="none"
+      viewBox="0 0 25 24"
+    >
+      <path
+        stroke="#298592"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M19.721 20.487v-.745a3 3 0 00-1.512-2.605l-3.219-1.842M10.448 15.293l-3.215 1.844a3 3 0 00-1.512 2.605v.745"
+      ></path>
+      <path
+        stroke="#298592"
+        strokeLinecap="square"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M12.72 16a4 4 0 01-4-4v-2a4 4 0 018 0v2a4 4 0 01-4 4z"
+      ></path>
+      <path
+        stroke="#298592"
+        strokeLinecap="square"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M12.72 23c6.076 0 11-4.925 11-11s-4.924-11-11-11c-6.074 0-11 4.925-11 11s4.926 11 11 11z"
+      ></path>
+    </svg>
+  );
+}
+
+export default UserIcon;
