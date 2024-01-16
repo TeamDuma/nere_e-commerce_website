c72ca82f1466b8c39ed03c6406becbd620f9a633
lo0oleta <alaa.alnous@gmail.com>
2024-01-16 19:19:56 +0000

    edit pickup location modal

--- src/lib/redux/services/location.ts
+++ src/lib/redux/services/location.ts
@@ -1,10 +1,10 @@
-import { GetlocationsResponse } from '@/types/location';
+import { ILocation } from '@/types/location';
 import { apiSlice } from '.';
 import endpoints from '../../endpoints';
 
 const locationsApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getlocations: builder.query<GetlocationsResponse, void>({
+    getlocations: builder.query<ILocation[], void>({
       query: () => {
         return endpoints.activeLocations;
       },
