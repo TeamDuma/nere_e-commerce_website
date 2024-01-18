f9179a18658961742f4e98b36532a88631701c18
lo0oleta <alaa.alnous@gmail.com>
2024-01-18 20:16:16 +0000

    add pointer to OngoingPurchase item

--- src/components/common/OngoingModal.tsx
+++ src/components/common/OngoingModal.tsx
@@ -23,9 +23,9 @@ const customStyles: Styles = {
     top: 0,
     left: 0,
     right: 'auto',
-    height: '100%',
+    height: '105%',
     width: '95%',
-    maxWidth: '400px',
+    maxWidth: '450px',
     margin: 'auto',
     backgroundColor: 'white',
     padding: '20px',
@@ -50,9 +50,9 @@ const OngoingModal: React.FC<{
       <h2
         style={{
           fontSize: '24px',
-          marginBottom: '20px',
           color: '#298592',
           fontWeight: 'bold',
+          marginTop: '20px',
         }}
       >
         Ongoing Purchases near me{' '}
