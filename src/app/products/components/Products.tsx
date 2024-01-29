9fb2f05d29116ea693c2a105543e5e38da488e4c
lo0oleta <alaa.alnous@gmail.com>
2024-01-29 11:05:37 +0000

    edit products screen and made it responsive

--- src/app/products/components/Products.tsx
+++ src/app/products/components/Products.tsx
@@ -8,7 +8,7 @@ const Products = () => {
   const products = data?.data?.products ?? [];
 
   const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 8; // Adjust as needed
+  const productsPerPage = 20; // Adjust as needed
 
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
@@ -19,77 +19,68 @@ const Products = () => {
 
   const renderProducts = currentProducts.map((product) => (
     <Link href={`/product/${product.id}`} key={product?.id}>
-      <Link href={`/product/${product.id}`}>
-        <div key={product?.id}>
-          <div
-            style={{
-              width: '40px',
-              height: '15px',
-              backgroundColor: '#F58929',
-              position: 'relative',
-              display: 'flex',
-              fontSize: '12px',
-              left: '146px',
-              top: '20px',
-              borderRadius: '8px',
-              justifyContent: 'center',
-              fontWeight: 'bold',
-              color: 'white',
-            }}
-          >
-            {` ${Math.round(
-              ((product.price - product.sale_price) / product.price) * 100
-            )}%`}
-          </div>
-          <div
-            style={{
-              backgroundSize: 'cover',
-              backgroundColor: '#F8F8F8',
-
-              width: '200px',
-              height: '200px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: '10px',
-            }}
-          >
-            <img
-              style={{ borderRadius: '10px' }}
-              src={product?.plain_image}
-              width={
-                product.name === 'Frytol sunflower oil 0.9L' ||
-                product.name === "Dr. Annie's honey 500ml"
-                  ? '60px'
-                  : '90px'
-              }
-              alt='cerelac image'
-            />
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <div className='product-name'>{product?.name}</div>
-            <div style={{ display: 'flex', marginTop: '2px' }}>
-              <div style={{ color: '#F31748', marginLeft: '5px' }}>
-                {`¢ ${product?.sale_price}`}
+      <div key={product?.id}>
+        <>
+          <div className='px-4 md:px-4 lg:px-4'>
+            <div className='relative rounded-md bg-gray-50 dark:bg-gray-800'>
+              <div className='h-15 absolute right-2 top-2 flex w-10 items-center justify-center rounded-md bg-[#F58929] text-xs font-bold text-white'>
+                {`${Math.round(
+                  ((product.price - product.sale_price) / product.price) * 100
+                )}%`}
               </div>
-              <div
-                style={{
-                  marginLeft: '14px',
-                  color: '#B3B3B3',
-                  textDecoration: 'line-through',
-                }}
-              >
-                {`¢ ${product?.price}`}
+
+              <div className='mt-8 flex items-center justify-center md:mt-24'>
+                <div
+                  style={{
+                    backgroundSize: 'cover',
+                    backgroundColor: '#F8F8F8',
+                    width: '200px',
+                    height: '200px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: '10px',
+                  }}
+                >
+                  <img
+                    style={{ borderRadius: '10px' }}
+                    src={product?.plain_image}
+                    width={
+                      product.name === 'Frytol sunflower oil 0.9L' ||
+                      product.name === "Dr. Annie's honey 500ml"
+                        ? '60px'
+                        : '90px'
+                    }
+                    alt='cerelac image'
+                  />
+                </div>
+              </div>
+              <div className='flex items-center'>
+                <h2
+                  tabIndex={0}
+                  className='product-name text-lg font-semibold focus:outline-none lg:text-lg lg:font-semibold'
+                >
+                  {product?.name}
+                </h2>
+              </div>
+              <div className='ml-2 flex'>
+                <div
+                  style={{ color: '#F31748' }}
+                >{`¢ ${product?.sale_price}`}</div>
+                <div
+                  style={{
+                    marginLeft: '14px',
+                    color: '#B3B3B3',
+                    textDecoration: 'line-through',
+                  }}
+                >
+                  {`¢ ${product?.price}`}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Link>{' '}
+        </>
+      </div>
     </Link>
   ));
 
@@ -116,7 +107,7 @@ const Products = () => {
     <div>
       <div className='container'>
         {isLoading && <div>Loading...</div>}
-        <div className='lg:grid-col-3 grid grid-cols-2  gap-10 sm:grid-cols-2 sm:place-items-start xl:grid-cols-4 xl:gap-x-20 xl:gap-y-10'>
+        <div className='lg:grid-col-4 grid grid-cols-2  sm:grid-cols-2 sm:place-items-start xl:grid-cols-4 xl:gap-x-10 xl:gap-y-5'>
           {renderProducts}
         </div>
         <ul className='pagination'>{renderPagination}</ul>
