7734c4d33383d5bcee79a3da96dc82062ed07b4d
lo0oleta <alaa.alnous@gmail.com>
2024-02-13 11:56:06 +0000

    edit products page to hide the category if its empty

--- src/app/categories/CategoriesRow.tsx
+++ src/app/categories/CategoriesRow.tsx
@@ -36,7 +36,88 @@ const CategoriesRow = ({ params }: Props) => {
   return (
     <>
       <div>
-        <div className='container'>
+      {products.length > 0 ? (
+  <div>
+    <div className='container'>
+      {isLoading && <div>Loading...</div>}
+      <Link href={`/category/${slug}`}>
+        <div className='flex items-center'>
+          <SubTitle text={subTitleText} />
+          <FaLongArrowAltRight style={{ marginLeft: 10, color: '#298592' }} />
+        </div>
+      </Link>
+      <div className='overflow-x-auto'>
+        <div className='flex flex-nowrap justify-start'>
+          {products.map((product) => (
+            <Link href={`/product/${product.slug}`} key={product?.id}>
+              <div
+                key={product?.id}
+                className='relative rounded-md bg-gray-50 dark:bg-gray-800'
+              >
+                <div className='h-15 absolute right-2 top-2 flex w-10 items-center justify-center rounded-md bg-[#F58929] text-xs font-bold text-white'>
+                  {`${Math.round(
+                    ((product.price - product.sale_price) / product.price) *
+                      100
+                  )}%`}
+                </div>
+                <div className='mt-2 flex items-center justify-center md:mt-4'>
+                  <div
+                    style={{
+                      backgroundSize: 'cover',
+                      backgroundColor: '#F8F8F8',
+                      width: '200px',
+                      height: '200px',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <img
+                      style={{ borderRadius: '10px' }}
+                      src={product?.plain_image}
+                      width={
+                        product.name === 'Frytol sunflower oil 0.9L' ||
+                        product.name === "Dr. Annie's honey 500ml"
+                          ? '60px'
+                          : '60px'
+                      }
+                      alt='cerelac image'
+                    />
+                  </div>
+                </div>
+                <div className='flex items-center'>
+                  <h2
+                    tabIndex={0}
+                    className='product-name text-lg font-semibold focus:outline-none lg:text-lg lg:font-semibold'
+                  >
+                    {product?.name}
+                  </h2>
+                </div>
+                <div className='ml-2 flex'>
+                  <div
+                    style={{ color: '#F31748' }}
+                  >{`¢ ${product?.sale_price}`}</div>
+                  <div
+                    style={{
+                      marginLeft: '14px',
+                      color: '#B3B3B3',
+                      textDecoration: 'line-through',
+                    }}
+                  >
+                    {`¢ ${product?.price}`}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+) : null}
+
+        {/* <div className='container'>
           {isLoading && <div>Loading...</div>}
           <Link href={`/category/${slug}`}>
             <div className='flex items-center'>
@@ -113,7 +194,7 @@ const CategoriesRow = ({ params }: Props) => {
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
