91358725106c858b96c80ff62f33778051277367
lo0oleta <alaa.alnous@gmail.com>
2024-02-28 18:15:05 +0000

    delete the button in the slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -45,7 +45,6 @@ const Slider = () => {
         ))}
       </div>
 
-
       <button
         type='button'
         className='group absolute left-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none'
@@ -55,8 +54,7 @@ const Slider = () => {
             prevIndex === 0 ? images.length - 1 : prevIndex - 1
           )
         }
-      >
-      </button>
+      ></button>
 
       <button
         type='button'
