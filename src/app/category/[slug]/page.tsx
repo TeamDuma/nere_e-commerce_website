2f3dbec1f850e753eb4c5efb69bf20214a6d469a
lo0oleta <alaa.alnous@gmail.com>
2024-02-28 14:25:17 +0000

    edit quick add design

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -68,120 +68,117 @@ const Category = ({ params }: Props) => {
           ) : (
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 xl:gap-x-5 '>
               {products.map((product) => (
-                    <Link href={`/product/${product.slug}`} key={product?.id}>
+                <Link href={`/product/${product.slug}`} key={product?.id}>
+                  <div
+                    key={product?.id}
+                    onMouseEnter={() => setHoveredProductId(String(product.id))}
+                    onMouseLeave={() => setHoveredProductId(null)}
+                    className='relative mx-8 rounded-md bg-gray-100 dark:bg-gray-800'
+                  >
+                    <div className='h-15 absolute right-2 top-2 flex w-10 items-center justify-center rounded-md bg-[#F58929] text-xs font-bold text-white'>
+                      {`${Math.round(
+                        ((product.price - product.sale_price) / product.price) *
+                          100
+                      )}%`}
+                    </div>
+                    <div className='mt-2 flex items-center justify-center md:mt-4'>
                       <div
-                        key={product?.id}
-                        onMouseEnter={() =>
-                          setHoveredProductId(String(product.id))
-                        }
-                        onMouseLeave={() => setHoveredProductId(null)}
-                        className='relative mx-8 rounded-md bg-gray-100 dark:bg-gray-800'
+                        style={{
+                          position: 'relative',
+                          backgroundSize: 'cover',
+                          backgroundColor: 'gray-100',
+                          width: '200px',
+                          height: '200px',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          borderRadius: '10px',
+                        }}
                       >
-                        <div className='h-15 absolute right-2 top-2 flex w-10 items-center justify-center rounded-md bg-[#F58929] text-xs font-bold text-white'>
-                          {`${Math.round(
-                            ((product.price - product.sale_price) /
-                              product.price) *
-                              100
-                          )}%`}
-                        </div>
-                        <div className='mt-2 flex items-center justify-center md:mt-4'>
+                        <img
+                          style={{ borderRadius: '10px' }}
+                          src={product?.plain_image}
+                          width={
+                            product.name === 'Frytol sunflower oil 0.9L' ||
+                            product.name === "Dr. Annie's honey 500ml"
+                              ? '60px'
+                              : '60px'
+                          }
+                          alt='cerelac image'
+                        />
+                        {hoveredProductId === String(product.id) && (
                           <div
                             style={{
-                              position: 'relative',
-                              backgroundSize: 'cover',
-                              backgroundColor: 'gray-100',
-                              width: '200px',
-                              height: '200px',
+                              position: 'absolute',
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              bottom: 0,
                               display: 'flex',
                               justifyContent: 'center',
                               alignItems: 'center',
+                              backgroundColor: 'rgba(0, 0, 0, 0.5)',
                               borderRadius: '10px',
                             }}
                           >
-                            <img
-                              style={{ borderRadius: '10px' }}
-                              src={product?.plain_image}
-                              width={
-                                product.name === 'Frytol sunflower oil 0.9L' ||
-                                product.name === "Dr. Annie's honey 500ml"
-                                  ? '60px'
-                                  : '60px'
-                              }
-                              alt='cerelac image'
-                            />
-                            {hoveredProductId === String(product.id) && (
-                              <div
-                                style={{
-                                  position: 'absolute',
-                                  top: 0,
-                                  left: 0,
-                                  right: 0,
-                                  bottom: 0,
-                                  display: 'flex',
-                                  justifyContent: 'center',
-                                  alignItems: 'center',
-                                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                                  borderRadius: '10px',
-                                }}
-                              >
-                                <button
-                                  className=' mx-6 my-4 rounded-md px-8  py-2 text-sm font-medium text-white hover:bg-[#D47826] focus:bg-[#D47826] focus:outline-none'
-                                  style={{ zIndex: 1 }}
-                                  onClick={(e) => {
-                                    e.preventDefault();
-                                    console.log(product.id);
-                                    handleAddToCart(product);
-                                    toast.success(
-                                      `${product.name} added to cart!`,
-                                      {
-                                        autoClose: 500,
-                                      }
-                                    );
-                                    sendGTMEvent({
-                                      event: 'categoryProductClicked',
-                                      value: `${product.name}`,
-                                    });
-                                    sendGAEvent({
-                                      event: 'categoryProductClicked',
-                                      value: `${product.name}`,
-                                    });                        
-                                            }}
-                                >
-                                  Quick Add
-                                </button>
-                              </div>
-                            )}
-                          </div>
-                        </div>
-
-                        <div className='flex items-center'>
-                          <div className='h-12'>
-                            <h5
-                              tabIndex={0}
-                              className='text ml-1  line-clamp-2 overflow-hidden overflow-ellipsis  text-[#298592]'
-                              style={{ maxWidth: '12rem' }}
+                            <button
+                              className=' mx-6 my-4 rounded-md px-8  py-2 text-sm font-medium text-white hover:bg-[#D47826] focus:bg-[#D47826] focus:outline-none'
+                              style={{ zIndex: 1 }}
+                              onClick={(e) => {
+                                e.preventDefault();
+                                console.log(product.id);
+                                handleAddToCart(product);
+                                toast.success(
+                                  `${product.name} added to cart!`,
+                                  {
+                                    autoClose: 500,
+                                  }
+                                );
+                                sendGTMEvent({
+                                  event: 'categoryProductClicked',
+                                  value: `${product.name}`,
+                                });
+                                sendGAEvent({
+                                  event: 'categoryProductClicked',
+                                  value: `${product.name}`,
+                                });
+                              }}
                             >
-                              {product?.name}
-                            </h5>
+                              Quick Add
+                            </button>
                           </div>
-                        </div>
-                        <div className='ml-2 flex'>
-                          <div
-                            style={{ color: '#F31748' }}
-                          >{`¢ ${product?.sale_price}`}</div>
-                          <div
-                            style={{
-                              marginLeft: '14px',
-                              color: '#B3B3B3',
-                              textDecoration: 'line-through',
-                            }}
-                          >
-                            {`¢ ${product?.price}`}
-                          </div>
-                        </div>
+                        )}
                       </div>
-                    </Link>
-                  ))}
+                    </div>
+
+                    <div className='flex items-center'>
+                      <div className='h-12'>
+                        <h5
+                          tabIndex={0}
+                          className='text ml-1  line-clamp-2 overflow-hidden overflow-ellipsis  text-[#298592]'
+                          style={{ maxWidth: '12rem' }}
+                        >
+                          {product?.name}
+                        </h5>
+                      </div>
+                    </div>
+                    <div className='ml-2 flex'>
+                      <div
+                        style={{ color: '#F31748' }}
+                      >{`¢ ${product?.sale_price}`}</div>
+                      <div
+                        style={{
+                          marginLeft: '14px',
+                          color: '#B3B3B3',
+                          textDecoration: 'line-through',
+                        }}
+                      >
+                        {`¢ ${product?.price}`}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
             </div>
           )}
         </div>
