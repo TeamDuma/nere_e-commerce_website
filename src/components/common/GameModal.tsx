a9a7a5005e06abf79c4f6476a8234677f9b1051a
lo0oleta <alaa.alnous@gmail.com>
2024-03-01 07:58:46 +0000

    add zIndex to the EntertainmentSection modals

--- src/components/common/GameModal.tsx
+++ src/components/common/GameModal.tsx
@@ -58,8 +58,7 @@ const customStylesSmall: Styles = {
     right: 0,
     bottom: 0,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex:1001
-
+    zIndex: 1001,
   },
   content: {
     display: 'flex',
