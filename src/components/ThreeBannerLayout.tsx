542d2b9b7e8170155db2cd39407d01648945ce97
lo0oleta <alaa.alnous@gmail.com>
2024-03-08 00:17:15 +0000

    edit FeaturedProducts page design

--- src/components/ThreeBannerLayout.tsx
+++ src/components/ThreeBannerLayout.tsx
@@ -36,14 +36,6 @@ const ThreeBannerLayout = () => {
           </div>
         </div>
       </section>
-
-      {/* <section className='m-3 rounded'>
-        <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3'>
-          <div className='m-4 flex items-center justify-between rounded-md border border-[#D9D9D9] bg-[#D9D9D9] p-16 shadow'></div>
-          <div className='m-4 flex items-center justify-between rounded-md border border-[#D9D9D9] bg-[#D9D9D9] p-16 shadow'></div>
-          <div className='m-4 flex items-center justify-between rounded-md border border-[#D9D9D9] bg-[#D9D9D9] p-16 shadow'></div>
-        </div>
-      </section> */}
     </>
   );
 };
