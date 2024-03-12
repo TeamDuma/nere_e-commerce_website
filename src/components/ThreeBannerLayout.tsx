f1f919ca5da69807ed3efdcfe94b6afcf82fbacf
lo0oleta <alaa.alnous@gmail.com>
2024-03-12 15:58:38 +0000

    edit LoyaltyModal

--- src/components/ThreeBannerLayout.tsx
+++ src/components/ThreeBannerLayout.tsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+import LoyaltyModal from './common/LoyaltyModal';
+
 const ThreeBannerLayout = () => {
+  const [LoyaltyVisible, setLoyaltyVisible] = useState(false);
+
+  const openLoyalty = () => {
+    setLoyaltyVisible(true);
+  };
+
+  const closeModal = () => {
+    setLoyaltyVisible(false);
+  };
+
   return (
     <>
       <div className='overflow-x-auto'>
@@ -13,7 +26,11 @@ const ThreeBannerLayout = () => {
             </div>
           </div>
           <div className='flex-shrink-0 cursor-pointer rounded'>
-            <div className='m-2' style={{ width: '460px', height: '170px' }}>
+            <div
+              className='m-2'
+              style={{ width: '460px', height: '170px' }}
+              onClick={openLoyalty}
+            >
               <img
                 src='/images/coupon2.svg'
                 alt='Your Image'
@@ -31,6 +48,7 @@ const ThreeBannerLayout = () => {
             </div>
           </div>
         </div>
+        <LoyaltyModal onClose={closeModal} isOpen={LoyaltyVisible} />
       </div>
     </>
   );
