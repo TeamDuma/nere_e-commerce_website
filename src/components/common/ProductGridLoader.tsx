f0f9951e02d02afe2db0b5ae96cebf0a80c50ae6
John Isutsah <johnisutsa@gmail,com>
2024-03-15 19:44:01 +0300

    feat: add loader on categories page

--- src/components/common/ProductGridLoader.tsx
+++ src/components/common/ProductGridLoader.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const ProductGridLoader: React.FC = () => {
-    return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-            {Array(12)
-                .fill(null)
-                .map((_, index) => (
-                    <div key={index} className='animate-pulse'>
-                        <div className='h-48 bg-gray-300 rounded'></div>
-                        <div className='h-4 mt-2 bg-gray-300 rounded w-3/4'></div>
-                        <div className='h-4 mt-2 bg-gray-300 rounded w-1/2'></div>
-                    </div>
-                ))}
-        </div>
-    );
+  return (
+    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      {Array(12)
+        .fill(null)
+        .map((_, index) => (
+          <div key={index} className='animate-pulse'>
+            <div className='h-48 rounded bg-gray-300'></div>
+            <div className='mt-2 h-4 w-3/4 rounded bg-gray-300'></div>
+            <div className='mt-2 h-4 w-1/2 rounded bg-gray-300'></div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ProductGridLoader;
