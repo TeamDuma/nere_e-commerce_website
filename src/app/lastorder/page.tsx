db4b68c750641b17f894044c968282d6440b1cf8
John Isutsah <johnisutsa@gmail,com>
2024-03-20 22:21:01 +0300

    [NT-174] feat: add message for when the order list is empty

--- src/app/lastorder/page.tsx
+++ src/app/lastorder/page.tsx
@@ -15,6 +15,7 @@ import { MdGroups, MdOutlineAccessAlarms } from 'react-icons/md';
 import GroupItem from '../checkout/summary/components/GroupItem';
 import OrderSummary from '../checkout/summary/components/OrderSummary';
 import ShareModal from '../checkout/summary/components/ShareModal';
+import Link from 'next/link';
 
 const Orders = () => {
   const { userInfo } = useSelector(selectShopping);
@@ -95,21 +96,34 @@ const Orders = () => {
   return (
     <>
       <div className='flex h-full w-screen flex-col px-8 py-7 md:flex-row'>
-        <div className='flex h-fit w-full flex-col gap-4'>
-          <p className='text-xl font-extrabold text-[#298592]'>
-            Congrats! 🎉 You Saved GHC {totalSavings.toFixed(2)} on your basket
-          </p>
-          {latestOrderItems.map((group) => (
-            <GroupItem
-              key={group.id}
-              group={group as Group}
-              showModal={handleShowModal}
-            />
-          ))}
-        </div>
-        <div className='flex h-fit w-full flex-col gap-4 p-4 '>
-          <OrderSummary amount={total} totalItems={totalItems} />
-        </div>
+        {latestOrderItems.length === 0 ? (
+          <div className='flex w-full flex-col items-center justify-center font-bold text-5xl'>
+            No Orders Yet!
+            <Link href='/products'>
+              <h1 className='my-5 cursor-pointer text-[#F58929] mt-4 font-normal text-xl hover:text-[#F58929] hover:underline hover:underline-offset-2'>
+                Go to the Shop
+              </h1>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <div className='flex h-fit w-full flex-col gap-4'>
+              <p className='text-xl font-extrabold text-[#298592]'>
+                Congrats! 🎉 You Saved GHC {totalSavings.toFixed(2)} on your basket
+              </p>
+              {latestOrderItems.map((group) => (
+                <GroupItem
+                  key={group.id}
+                  group={group as Group}
+                  showModal={handleShowModal}
+                />
+              ))}
+            </div>
+            <div className='flex h-fit w-full flex-col gap-4 p-4 '>
+              <OrderSummary amount={total} totalItems={totalItems} />
+            </div>
+          </>
+        )}
 
         {/* Modal */}
         {showModal && (
--- src/app/profile/components/OrderContent.tsx
+++ src/app/profile/components/OrderContent.tsx
@@ -7,6 +7,7 @@ import { useLazyGetOrdersQuery } from '@/lib/redux/services/customers';
 import { Orders } from '@/types/orders';
 import { useSelector } from 'react-redux';
 import { selectShopping } from '@/lib/redux/slices/shopping';
+import Link from 'next/link';
 
 const OrderContent = () => {
   const { userInfo } = useSelector(selectShopping);
@@ -28,13 +29,31 @@ const OrderContent = () => {
       <div>
         {isLoading && <p>Loading...</p>}
         {isError && <p style={{ color: 'red' }}>Error</p>}
-        {orders && (
+        {orders && orders.length > 0 ? (
           <div>
             {orders.map((item: Orders) => (
               <div key={item.id}>
                 <OrderContentCard item={item} />
               </div>
             ))}
+          </div>
+        ) : (
+          // <div className='flex flex-col items-center justify-center'>
+
+          //   <p className='text-6xl font-semibold'>No Orders Yet!</p>
+          //   <p
+          //     onClick={() => window.location.replace('/products')}
+          //     className='text-[#298592] cursor-pointer underline-offset-2 underline hover:text-[#1A464C] hover:underline-offset-2 hover:underline mt-4'
+          //   >Start Shopping!</p>
+          // </div>
+
+          <div className='text-center text-5xl text-black font-semibold'>
+            No Orders Yet!
+            <Link href='/products'>
+              <h1 className='my-5 cursor-pointer text-[#F58929] mt-4 font-normal text-xl hover:text-[#F58929] hover:underline hover:underline-offset-2'>
+                Go to the Shop
+              </h1>
+            </Link>
           </div>
         )}
       </div>
