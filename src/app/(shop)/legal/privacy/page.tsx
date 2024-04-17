7fd661dcc53686c464b51569b89247d31a20859d
lo0oleta <alaa.alnous@gmail.com>
2024-04-17 21:45:08 +0300

    add watermark in legal folder pages

--- src/app/(shop)/legal/privacy/page.tsx
+++ src/app/(shop)/legal/privacy/page.tsx
@@ -1,4 +1,4 @@
-"use client" 
+'use client';
 import TableOfContents from './components/TableOfContents';
 import Watermark from '@uiw/react-watermark';
 const Page = () => {
@@ -6,28 +6,28 @@ const Page = () => {
     <>
       {/* component */}
       <Watermark
-  height={400}
-  width={600}
-  image="https://res.cloudinary.com/dc0q9vdbb/image/upload/v1713378960/Group_14023_1_oy6qvc.png"
-    >
-      <div>
-        <div className='mx-auto mt-6 max-w-7xl px-4 sm:px-2 lg:px-8'>
-          <div className='grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible'>
-            <div className='flex flex-col gap-8 text-left'>
-              <h1 className='text-4xl	font-bold	text-[#298592]'>
-                NERE COMMUNITY PRIVACY NOTICE
-              </h1>
+        height={400}
+        width={600}
+        image='https://res.cloudinary.com/dc0q9vdbb/image/upload/v1713378960/Group_14023_1_oy6qvc.png'
+      >
+        <div>
+          <div className='mx-auto mt-6 max-w-7xl px-4 sm:px-2 lg:px-8'>
+            <div className='grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible'>
+              <div className='flex flex-col gap-8 text-left'>
+                <h1 className='text-4xl	font-bold	text-[#298592]'>
+                  NERE COMMUNITY PRIVACY NOTICE
+                </h1>
+              </div>
             </div>
+            <div className='ml-12  flex'>
+              <h2 className='ml-5	text-3xl	font-bold text-[#298592]'>
+                Table of content
+              </h2>
+            </div>
+            <TableOfContents />
           </div>
-          <div className='ml-12  flex'>
-            <h2 className='ml-5	text-3xl	font-bold text-[#298592]'>
-              Table of content
-            </h2>
-          </div>
-          <TableOfContents />
         </div>
-      </div>
-   </Watermark>
+      </Watermark>
     </>
   );
 };
