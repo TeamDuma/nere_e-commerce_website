eedef4c9d7ca54aaea271d2960eb8af732ebfbe2
lo0oleta <alaa.alnous@gmail.com>
2024-04-17 21:44:29 +0300

    add watermark

--- src/app/(shop)/legal/terms-of-service/page.tsx
+++ src/app/(shop)/legal/terms-of-service/page.tsx
@@ -1,9 +1,15 @@
+"use client" 
 import TableOfContents from './components/TableOfContents';
-
+import Watermark from '@uiw/react-watermark';
 const Page = () => {
   return (
     <>
       {/* component */}
+      <Watermark
+  height={400}
+  width={600}
+  image="https://res.cloudinary.com/dc0q9vdbb/image/upload/v1713378960/Group_14023_1_oy6qvc.png"
+    >
       <div>
         <div className='mx-auto mt-6 max-w-7xl px-4 sm:px-2 lg:px-8'>
           <div className='grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible'>
@@ -26,6 +32,7 @@ const Page = () => {
           <TableOfContents />
         </div>
       </div>
+      </Watermark>
     </>
   );
 };
