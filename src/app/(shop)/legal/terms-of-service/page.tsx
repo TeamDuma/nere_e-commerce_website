5787d387f78b3cbfb79e60f50e8bddc2af3738f9
John Isutsah <johnisutsa@gmail,com>
2024-04-22 10:41:37 +0300

    edited terms of service page

--- src/app/(shop)/legal/terms-of-service/page.tsx
+++ src/app/(shop)/legal/terms-of-service/page.tsx
@@ -15,10 +15,10 @@ const Page = () => {
             <div className='grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible'>
               <div className='flex flex-col gap-8 text-left'>
                 <h1 className='text-4xl	font-bold	text-[#298592]'>
-                  Nere Community Terms of Service- Contents
+                  Nere Community Terms of Service
                 </h1>
                 <p>
-                  Welcome message: Welcome to Nere Community! We’re happy to
+                  Welcome to Nere Community! We’re happy to
                   have you on board. We hope that you enjoy your experience!
                   Please be sure to familiarize yourself with our terms stated
                   below.
