208bd8d5eb41168c44b0a67bab24c2942168a8e6
lo0oleta <alaa.alnous@gmail.com>
2024-04-24 19:23:12 +0300

    Join Whatsapp Banner

--- src/components/common/WhatsappCommunityBanner.tsx
+++ src/components/common/WhatsappCommunityBanner.tsx
@@ -6,15 +6,16 @@ const WhatsappCommunityBanner = () => {
   return (
     <>
       <div className='overflow-x-auto'>
-        <div className='mb-12 flex justify-center'>
-          <div className='flex items-center'>
-            <ArrowIcon />
+        <div className=' flex justify-center pb-1 '>
+          <ArrowIcon />
+
+          <div className='flex items-center '>
             <a
               className='hover:opacity-75'
               href='https://chat.whatsapp.com/GPWialGrINjHtW4tJyzqEt'
               target='_blank'
             >
-              <div className='mt-16 flex flex-col items-center justify-center rounded-2xl bg-[#FCF5E8] p-4 text-center md:mr-8 md:flex-row'>
+              <div className=' flex flex-col items-center justify-center rounded-2xl bg-[#FCF5E8] p-4 text-center md:mr-8 md:flex-row'>
                 <BsWhatsapp className='mr-2' color='#25D366' fontSize={24} />
                 <span className='text-lg font-bold text-[#0E464E]'>
                   Join our WhatsApp Community 🥳
