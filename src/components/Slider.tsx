3eedc2e18d8e44611bbeb95ecd9aca5c3b83e560
lo0oleta <alaa.alnous@gmail.com>
2024-04-26 02:20:57 +0300

    edit Banner page remove use client

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,7 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 const Banner = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentIndexRef = useRef(0);
+  const [, setTick] = useState(0); // Dummy state for triggering re-renders
   const images = [
     '../images/nere_sunflower.png',
     '../images/nere_breakfast.png',
@@ -11,43 +9,40 @@ const Banner = () => {
   ];
 
   useEffect(() => {
-    const moveSlideTimeout = setTimeout(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    const moveSlideInterval = setInterval(() => {
+      currentIndexRef.current = (currentIndexRef.current + 1) % images.length;
+      setTick((tick) => tick + 1);
     }, 7000);
 
     return () => {
-      clearTimeout(moveSlideTimeout);
+      clearInterval(moveSlideInterval);
     };
-  }, [currentIndex]);
+  }, []);
+
+  const handlePrevButtonClick = () => {
+    currentIndexRef.current =
+      currentIndexRef.current === 0
+        ? images.length - 1
+        : currentIndexRef.current - 1;
+    setTick((tick) => tick + 1);
+  };
+
+  const handleNextButtonClick = () => {
+    currentIndexRef.current = (currentIndexRef.current + 1) % images.length;
+    setTick((tick) => tick + 1);
+  };
 
   return (
-    <div className='relative  mx-auto w-full'>
+    <div className='relative mx-auto w-full'>
       <div
         id='default-carousel'
         className='relative mb-6 h-32 overflow-hidden rounded-xl sm:h-72 xl:h-96 2xl:h-96'
       >
-        {/* {images.map((image, index) => (
-          <div
-            key={index}
-            className={`duration-700 ease-in-out${
-              index === currentIndex ? '' : ' hidden'
-            }`}
-            data-carousel-item=''
-          >
-            <img
-              style={{ borderRadius: 10 }}
-              width='100%'
-              src={image}
-              className='absolute left-1/2 top-1/2 block w-fit  -translate-x-1/2 -translate-y-1/2 '
-              alt={`Slide ${index + 1}`}
-            />
-          </div>
-        ))} */}
         {images.map((image, index) => (
           <div
             key={index}
             className={`duration-700 ease-in-out ${
-              index === currentIndex ? '' : ' hidden'
+              index === currentIndexRef.current ? '' : 'hidden'
             }`}
             data-carousel-item=''
           >
@@ -62,22 +57,16 @@ const Banner = () => {
 
       <button
         type='button'
-        className='group absolute left-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none'
+        className='group absolute left-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none '
         data-carousel-prev=''
-        onClick={() =>
-          setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? images.length - 1 : prevIndex - 1
-          )
-        }
+        onClick={handlePrevButtonClick}
       ></button>
 
       <button
         type='button'
         className='group absolute right-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none'
         data-carousel-next=''
-        onClick={() =>
-          setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
-        }
+        onClick={handleNextButtonClick}
       >
         {/* Next button content */}
       </button>
