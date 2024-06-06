fe149dd85630288ad5c278f746248f8974e5dd5f
Derek <reign.derek@yahoo.com>
2024-06-06 19:45:49 +0000

    Add posthog wrapper

--- src/app/posthog.ts
+++ src/app/posthog.ts
@@ -1,8 +1,36 @@
 import { PostHog } from 'posthog-node';
 
 export default function PostHogClient() {
-  const postHogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  });
-  return postHogClient;
+  if (process.env.NODE_ENV !== 'development') {
+    const postHogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    });
+    return postHogClient;
+  }
+  return null;
 }
+
+interface IdentifyMessage {
+  distinctId: string;
+  properties?: Record<string | number, any>;
+  disableGeoip?: boolean;
+}
+interface EventMessage extends IdentifyMessage {
+  event: string;
+  groups?: Record<string, string | number>;
+  sendFeatureFlags?: boolean;
+  timestamp?: Date;
+  uuid?: string;
+}
+
+export const isProduction = process.env.NODE_ENV === 'production';
+
+const postHogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+});
+
+export const posthogCapture = (props: EventMessage) => {
+  if (!isProduction) {
+    postHogClient.capture({ ...props });
+  }
+};
