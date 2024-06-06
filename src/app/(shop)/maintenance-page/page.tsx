bf3759b0cfaa9ca1116544c2d5d60cd802f41e55
lo0oleta <Alaa.alnous@gmail.com>
2024-06-06 14:10:33 +0300

    edit maintenancePage

--- src/app/(shop)/maintenance-page/page.tsx
+++ src/app/(shop)/maintenance-page/page.tsx
@@ -5,39 +5,36 @@ import Image from 'next/image';
 const Page = () => {
   return (
     <>
-    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 pb-12  flex justify-center flex-col-reverse lg:flex-row md:gap-28 ">
-      <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-        <div className="relative">
-          <div className="absolute">
-            <div className="">
-            <h1 className='my-2 text-lg	 font-bold text-[#1A464C] md:text-4xl text-center'>
-              Page under maintenance...
-              </h1>
-              <p className='my-2 text-sm text-gray-800 md:text-base  text-center '>
-              This page is currently under maintenance and will be back soon.
-
-              </p>
-              <button className='my-4 w-full rounded-full	 border bg-[#1A464C] px-8 py-3 text-center text-sm font-bold text-white hover:bg-[#1A464C] focus:outline-none focus:ring-2 focus:ring-[#298592] focus:ring-opacity-50 sm:w-auto md:px-24'>
-              Continue shopping
-              </button>
+      <div className='flex flex-col-reverse justify-center px-4 pb-12 md:gap-28  md:px-44 md:py-20 lg:flex-row lg:px-24 lg:py-24 '>
+        <div className='relative w-full pb-12 lg:pb-0 xl:w-1/2 xl:pt-24'>
+          <div className='relative'>
+            <div className='absolute'>
+              <div className=''>
+                <h1 className='my-2 text-center	 text-lg font-bold text-[#1A464C] md:text-4xl'>
+                  Page under maintenance...
+                </h1>
+                <p className='my-2 text-center text-sm text-gray-800  md:text-base '>
+                  This page is currently under maintenance and will be back
+                  soon.
+                </p>
+                <button className='my-4 w-full rounded-full	 border bg-[#1A464C] px-8 py-3 text-center text-sm font-bold text-white hover:bg-[#1A464C] focus:outline-none focus:ring-2 focus:ring-[#298592] focus:ring-opacity-50 sm:w-auto md:px-24'>
+                  Continue shopping
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="pb-32	">
+            <div className='pb-32	'></div>
           </div>
         </div>
+        <div>
+          <Image
+            src='/images/maintenance.svg'
+            width={400}
+            height={300}
+            alt={'Maintenance in progress Image'}
+          />
+        </div>
       </div>
-      <div>
-      <Image
-          src='/images/maintenance.svg'
-          width={400}
-          height={300}
-          alt={'Maintenance in progress Image'}
-        />
-      </div>
-    </div>
-  </>
-  
-  
+    </>
   );
 };
 
