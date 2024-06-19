159a99ec02e68ad73aab2fd171766150405707dc
Derek <reign.derek@yahoo.com>
2024-06-19 22:14:20 +0000

    Update FAQ accordion

---
+++ src/components/common/icons/MinusIcon.tsx
@@ -0,0 +1,18 @@
+export const MinusIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M5 12H19'
+      stroke='#3E3E3E'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
