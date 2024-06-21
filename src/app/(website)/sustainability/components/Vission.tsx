086de8348c9f92eef69d5214d009bd917453d0ed
Derek <reign.derek@yahoo.com>
2024-06-21 10:13:54 +0000

    Update images on sustainability page

--- src/app/(website)/sustainability/components/Vission.tsx
+++ src/app/(website)/sustainability/components/Vission.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Vision = () => {
   return (
     <section className='nere-container pb-20 pt-[60px] md:pb-[130px] md:pt-[110px]'>
-      <div className='grid-col-1 mb-12 grid gap-x-4 md:grid-cols-2'>
+      <div className='grid-col-1 mb-12 grid gap-x-4 md:mb-[110px] md:grid-cols-2'>
         <div className='order-2 md:order-1'>
           <Image
             src='/images/about/vission-1.png'
@@ -17,7 +17,7 @@ export const Vision = () => {
           <h1 className='mb-3 text-[24px] font-bold leading-[28.8px] text-nere-green md:text-[40px] md:leading-[48px]'>
             Empowering Local Businesses, Building a Stronger Community
           </h1>
-          <p className='mb-6 text-[14px] leading-[20px] md:text-[18px] md:leading-[32px]'>
+          <p className='text-[14px] leading-[20px] md:text-[18px] md:leading-[32px]'>
             Nere connects small businesses with a wider customer base through
             our group-buying platform. This allows them to reach new consumers,
             increase sales volume, and gain valuable market insights.
@@ -25,11 +25,11 @@ export const Vision = () => {
         </div>
       </div>
       <div className='grid-col-1 grid gap-x-4 md:grid-cols-2'>
-        <div className='my-auto w-full md:max-w-lg'>
+        <div className='my-auto w-full md:max-w-lg '>
           <h1 className='mb-3 text-[24px] font-bold leading-[28.8px] text-nere-green md:text-[40px] md:leading-[48px]'>
             Building a Sustainable Future, Together
           </h1>
-          <p className='mb-6 text-[14px] leading-[20px] md:pr-10 md:text-[18px] md:leading-[32px] lg:pr-20'>
+          <p className='text-[14px] leading-[20px] md:pr-10 md:text-[18px] md:leading-[32px] lg:pr-20'>
             At Nere, we believe in responsible growth. Our focus on group buying
             not only saves money, but also reduces overall consumption by
             eliminating excess packaging and wasted goods. Additionally, our
