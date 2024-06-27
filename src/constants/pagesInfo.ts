fb1803cd9734b320abfd1420d9fee951d1bce7c7
Derek <reign.derek@yahoo.com>
2024-06-27 17:52:44 +0000

    feat: track agent referral code

---
+++ src/constants/pagesInfo.ts
@@ -0,0 +1,18 @@
+import { ClickType } from '@/types/referral';
+
+interface IPagesInfo {
+  title: string;
+  clickType: ClickType;
+}
+
+export const PAGES_INFO: Record<string, IPagesInfo> = {
+  '/home': { title: 'Landing Page', clickType: ClickType.LANDING_PAGE },
+  '/sustainability': {
+    title: 'Landing Page',
+    clickType: ClickType.LANDING_PAGE,
+  },
+  '/contact-us': { title: 'Landing Page', clickType: ClickType.LANDING_PAGE },
+  '/products': { title: 'Product Page', clickType: ClickType.PRODUCTS_PAGE },
+  '/buy': { title: 'Buy Button', clickType: ClickType.BUY_BUTTON },
+  '/whatsapp': { title: 'Whatsapp', clickType: ClickType.WHATSAPP },
+};
