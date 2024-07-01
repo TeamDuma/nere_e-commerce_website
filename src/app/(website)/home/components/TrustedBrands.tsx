b6b297d1d9abfd1517f5d1cf71d3295cde2b3b87
Derek <reign.derek@yahoo.com>
2024-07-01 16:58:58 +0000

    chore: update trustedbrands marquee

--- src/app/(website)/home/components/TrustedBrands.tsx
+++ src/app/(website)/home/components/TrustedBrands.tsx
@@ -1,17 +1,10 @@
-import { AirbnbIcon } from '@/components/common/icons/AirbnbIcon';
-import { AmazonIcon } from '@/components/common/icons/AmazonIcon';
+import Marquee from 'react-fast-marquee';
 import { AutoSurveilIcon } from '@/components/common/icons/AutoSurveilIcon';
 import { BabyNestIcon } from '@/components/common/icons/BabyNestIcon';
 import { CaramelIcon } from '@/components/common/icons/CaramelIcon';
-import { CoinbaseIcon } from '@/components/common/icons/CoinbaseIcon';
 import { DexwinIcon } from '@/components/common/icons/DexwinIcon';
-import { GoogleIcon } from '@/components/common/icons/GoogleIcon';
 import { ImaniIcon } from '@/components/common/icons/ImaniIcon';
-import { SlackIcon } from '@/components/common/icons/SlackIcon';
-import { StripeIcon } from '@/components/common/icons/StripeIcon';
 import { SunnyIcon } from '@/components/common/icons/SunnyIcon';
-import { TwitchIcon } from '@/components/common/icons/TwitchIcon';
-import Marquee from 'react-fast-marquee';
 
 export const TrustedBrands = () => (
   <section className='nere-container pb-0 pt-[25px] md:pb-[25px]'>
@@ -21,13 +14,6 @@ export const TrustedBrands = () => (
       </p>
     </div>
     <Marquee gradient={false} speed={40}>
-      {/* <AirbnbIcon className='mr-12 md:mr-24' />
-      <SlackIcon className='mr-12 md:mr-24' />
-      <GoogleIcon className='mr-12 md:mr-24' />
-      <AmazonIcon className='mr-12 md:mr-24' />
-      <TwitchIcon className='mr-12 md:mr-24' />
-      <StripeIcon className='mr-12 md:mr-24' />
-      <CoinbaseIcon className='mr-12 md:mr-24' /> */}
       <AutoSurveilIcon className='mr-12 md:mr-24' />
       <DexwinIcon className='mr-10 md:mr-20' />
       <ImaniIcon className='mr-10 md:mr-20' />
