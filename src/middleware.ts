2a002dc1364217720add78f16ad103df3d985f0b
Derek <reign.derek@yahoo.com>
2024-07-30 18:12:12 +0000

    fix: ignore images in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next|static|favicon.ico).*)',
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
 };
