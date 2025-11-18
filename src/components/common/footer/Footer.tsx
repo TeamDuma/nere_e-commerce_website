e9f5df2eb7a1915e4329f5556f3a07392aa465a8
John Isutsah <johnisutsa@gmail.com>
2025-11-18 13:31:32 +0300

    chore: Update font. Remove tt-norms
    Signed-off-by: John Isutsah <johnisutsa@gmail.com>

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -85,7 +85,7 @@ const socialMediaLinks = [
 const Footer = () => {
   return (
     <>
-      <footer className="bg-[url('/images/styled-green-background.png')] bg-cover bg-center bg-no-repeat p-0 font-ttnorms">
+      <footer className="bg-[url('/images/styled-green-background.png')] bg-cover bg-center bg-no-repeat p-0 font-montserrat">
         <div className='nere-container pb-[101px] pt-[84px]'>
           <div className='flex flex-wrap gap-y-8 md:flex-nowrap'>
             <MainLogo
